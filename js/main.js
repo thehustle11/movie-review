fdccbf444b63e476b3bd15523d5136a082b6573c
thehustle11 <vivekvarma124@gmail.com>
2017-02-04 14:30:56 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('.menu').click(function(e) {
+    // e.stopPropogation();
+    $('#main_nav').toggleClass('active');
+  });
+  // 
+  // $(document).click(function() {
+  //   $('#main_nav').removeClass('active');
+  // });
+
+});
